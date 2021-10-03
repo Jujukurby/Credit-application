b12e53d6c0a7cc05203c8ea0b56181309dce3e18
Julien Dumortier <julien.dumortier@free.fr>
2021-10-03 15:57:37 +0200

    pie chart

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 import dash
 import dash.dcc as dcc
 import dash.html as html
+from dash.html.P import P
 import pandas as pd
 from dash.dependencies import Input, Output
 import xgboost as xgb
 import plotly.graph_objects as go
+import plotly.express as px
 
 app = dash.Dash(__name__, prevent_initial_callbacks=True)
 
@@ -35,6 +37,9 @@ app.layout = html.Div(
          placeholder="Select a client",
     ),
         html.P(
+            dcc.Graph(id='circle_proba')
+            ),
+        html.P(
             id='graph-with-client',
             ),
     ]
@@ -48,12 +53,32 @@ def update_output(selected_client):
     client_data = data.loc[selected_client,:]
     toplot = data[data['Probability_of_reemboursement'] >= 0.7]
     output = []
+    palette = px.colors.qualitative.G10
+    palette = palette + palette
+    i=0
     for col in  data.columns[0:-1]:
         fig = go.Figure()
-        fig = fig.add_histogram(x = toplot[col])
+        fig = fig.add_histogram(x = toplot[col], marker=dict(color=palette[i]) )
         fig = fig.add_vline(client_data[col])
+        fig = fig.update_layout(title=col)
         output.append(dcc.Graph(id=col,figure=fig))
     return output
+
+@app.callback(
+    Output('circle_proba', 'figure'),
+    Input(component_id='selected_client', component_property='value')
+)
+def circle_proba(selected_client):
+    client_data = data.loc[selected_client,:]
+    values = [client_data['Probability_of_reemboursement'], 1-client_data['Probability_of_reemboursement']]
+    fig = go.Figure(data=[go.Pie(values=values, hole = 0.4)])
+    # fig = fig.add_shape(type="circle",
+    # xref="x", yref="y",
+    # fillcolor="white",
+    # x0=0, y0=0, x1 = 0.5, y1=0.5,
+    # line_color="LightSeaGreen",
+    # )
+    return fig
 
 
 if __name__ == "__main__":
